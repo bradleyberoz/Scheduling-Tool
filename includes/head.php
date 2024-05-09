d8405a563661a05f9276e00ca12c15af0bb589fe
bradleyberoz <96027996+bradleyberoz@users.noreply.github.com>
2024-05-09 14:19:39 -0400

    Create head.php

---
+++ includes/head.php
@@ -0,0 +1,17 @@
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        
+        <!-- Bootstrap CSS -->
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+        
+        <!-- Bootstrap Icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+        
+        <!-- Journal Theme -->
+        <link rel="stylesheet" href="https://bootswatch.com/4/journal/bootstrap.min.css">
+        
+        <!-- Custom CSS -->
+        <link type='text/css' rel='stylesheet' href='css/magmaStyles.css' />
+        
+        <!-- AJAX script -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
